37e0c24d7259931e4724a84a7b94bda8d0806365
devcompany-public <=>
2023-06-27 22:47:06 +0300

    @v1.3.2

--- js/anim.js
+++ js/anim.js
@@ -1,77 +1,230 @@
-function cr_overlay() {
-  var overlay = cr_el("overlay");
-  return overlay;
-}
-function cr_el(name) {
+function cr_el(name, parent = document.body) {
   var el = document.createElement("div");
   el.className = name;
-  document.body.appendChild(el);
+  parent.appendChild(el);
   return el;
 }
 function rm_el(el) {
   el.remove();
 }
-function alerter(massage, duration = 2000) {
-  var el = cr_el("alerter");
+function cr_overlay(parent = document.body, ad_name) {
+  var overlay = cr_el(`overlay ${ad_name}`, parent);
+  return overlay;
+}
+function cr_style(href) {
+  var style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = `../css/themes/${href}.css`;
+  document.head.appendChild(style);
+}
+document.body.dataset.animateIt.split(" ").forEach((el) => {
+  cr_style(el);
+});
+function _alert(massage = "Thanks", duration = 2000, toggle = false) {
+  var el = cr_el("alerter --pop-up");
   el.innerHTML = massage;
-  animate(el, duration);
+  if (!toggle) {
+    _animate(el, duration);
+  } else {
+    _begin_animate(el);
+  }
+  return el;
 }
-function animate(el, duration = 2000) {
-  begin_animate(el);
-  leave_animate(el, duration);
+function _animate(el, duration = 2000) {
+  _begin_animate(el);
+  _leave_animate(el, duration);
 }
-function begin_animate(el, delay = 1) {
+function _begin_animate(el, delay = 1) {
   el.classList.add("--reset");
   setTimeout(() => {
     el.classList.add("--happening");
   }, delay);
 }
-function leave_animate(el, delay = 1) {
-  setTimeout(() => {
+function _leave_animate(el, delay = 1) {
+  const timeout = setTimeout(() => {
     el.classList.add("--leaving");
     setTimeout(() => {
       rm_el(el);
     }, 200);
   }, delay);
+  return timeout;
 }
-function pop_up(
-  subject = "confirming",
-  text = "please confirm this form.",
-  button = "ok"
+function cr_popup(
+  elClass = "",
+  data = {
+    subject,
+    text,
+    button,
+  }
 ) {
+  data.subject = _assign(data.subject, "Confirming");
+  data.text = _assign(data.text, "Please confim this form.");
+  data.button = _assign(data.button, "Ok");
   var overlay = cr_overlay();
-  begin_animate(overlay);
-  var pop_up = cr_el("pop_up");
+  _begin_animate(overlay);
+  var pop_up = cr_el(`--pop-up ${elClass}`);
   pop_up.innerHTML = `
-    <h2>${subject}</h2><p>${text}</p>
-    <div><a onclick="pop_up_close(this.parentElement.parentElement)">${button}</a></div>
+    <h2>${data.subject}</h2>
+    <p>${data.text}</p>
+    <div><a>${data.button}</a></div>
   `;
-  begin_animate(pop_up);
+  _begin_animate(pop_up);
+  return pop_up;
 }
-function pop_up_choice(
-  subject = "confirming",
-  text = "Please confirm",
-  button_1 = "cancel",
-  button_2 = "confirm",
+function _confirm_alert(
+  data = {
+    subject: "confirming",
+    text: "Please confirm this form",
+    button: "Ok",
+  }
+) {
+  data.subject = _assign(data.subject, "confirming");
+  data.text = _assign(data.text, "please confirm this form.");
+  data.button = _assign(data.button, "ok");
+  cr_popup(
+    "pop_up",
+    (data = { subject: data.subject, text: data.text, button: data.button })
+  );
+  document.querySelector(".pop_up a").addEventListener("click", _close_popup);
+}
+function _popup_choice(
+  data = {
+    subject: "Confirming",
+    text: "Please confirm",
+    button_1: "cancel",
+    button_2: "confirm",
+  },
   callback
 ) {
-  pop_up(subject, text, `${button_1}</a><a class="btn-confirm">${button_2}`);
+  data.subject = _assign(data.subject, "Confirming");
+  data.text = _assign(data.text, "Please confirm");
+  data.button_1 = _assign(data.button_1, "cancel");
+  data.button_2 = _assign(data.button_2, "confirm");
+  cr_popup("", {
+    subject: data.subject,
+    text: data.text,
+    button: `${data.button_1}</a><a class="btn-confirm">${data.button_2}`,
+  });
   var btn_confirm = document.querySelector(".btn-confirm");
-  btn_confirm.addEventListener("click", () => {
-    pop_up_close(btn_confirm.parentElement.parentElement);
+  btn_confirm.addEventListener("click", _close_popup);
+  btn_confirm.addEventListener("click", callback);
+  document
+    .querySelector(".--pop-up div a:first-child")
+    .addEventListener("click", _close_popup);
+}
+function _close_popup() {
+  _leave_animate(document.querySelector(".--pop-up"));
+  _leave_animate(document.querySelector(".overlay"));
+}
+function _prompt(
+  data = { subject: "Type in the form", button: "Send" },
+  callback
+) {
+  data.subject = _assign(data.subject, "Type in the form");
+  data.button = _assign(data.button, "Send");
+  cr_popup("--prompt", {
+    subject: data.subject,
+    text: "<input type='text' class='prompt-input' />",
+    button: data.button,
+  });
+  var a = document.querySelector(".--prompt a");
+  a.classList.add("--disabled");
+  var input = document.querySelector(".prompt-input");
+  input.addEventListener("keyup", (ev) => {
+    var canClose = false;
+    if (input.value !== "") {
+      canClose = true;
+      a.classList.remove("--disabled");
+    } else {
+      a.classList.add("--disabled");
+    }
+    if (ev.key == "Enter") {
+      a.click();
+    }
+  });
+  var _en_callback = () => {
+    if (a.className.indexOf("--disabled") <= -1) {
+      callback(input.value);
+      _close_popup();
+    }
+  };
+  a.addEventListener("click", _en_callback);
+}
+function _notify(
+  massage = "Thanks",
+  options = { duration: 5000, onHoverPause: true }
+) {
+  var notification = _alert(massage, 5000, true);
+  options.duration = _assign(options.duration, 5000);
+  options.onHoverPause = _assign(options.onHoverPause, true);
+  notification.classList.add("--notification");
+  var duration = options.duration;
+  notification.style.setProperty(
+    "--notification-after-duration",
+    `${duration}ms`
+  );
+  var newDuration,
+    delay = Date.now();
+  duration += 29;
+  var timer = _leave_animate(notification, duration);
+  var once = true;
+  var overlay;
+  if (options.onHoverPause) {
+    notification.classList.add("--on-hover-pause");
+    notification.addEventListener("mouseover", () => {
+      clearTimeout(timer);
+      if (once) {
+        newDuration = duration - (Date.now() - delay);
+        overlay = cr_overlay(notification, "--overlay-inboxed");
+        var x_span = document.createElement("span");
+        x_span.className = "x-span";
+        overlay.appendChild(x_span);
+        once = false;
+        overlay.addEventListener("click", () => {
+          _leave_animate(notification);
+        });
+      }
+    });
+    notification.addEventListener("mouseleave", () => {
+      timer = _leave_animate(notification, newDuration);
+      rm_el(overlay);
+      once = true;
+      delay = Date.now();
+      duration = newDuration;
+    });
+  }
+}
+function _null(txt) {
+  if (txt == null) return true;
+  else return false;
+}
+function _assign(txt_1, txt_2) {
+  if (_null(txt_1)) return txt_2;
+  else return txt_1;
+}
+function _log(logCommand, callback) {
+  var actions = logCommand.split(",");
+  var logParams = actions[0].split(":");
+  var mainCommand = logParams[0];
+  var mainContext = logParams[1].split(",")[0],
+    wiki = "";
+  if (actions[1]) {
+    var wikiLink = actions[1].trim().split("wiki:")[1];
+    console.log(actions[1].trim().split("wiki:"));
+    // if (wikiLink.trim() == "http" || wikiLink.trim() == "https") {
+    //   wikiLink += actions[1].split(":")[2];
+    // }
+    wiki = `<hr> You can know more about that in <a class="link" href=${wikiLink}>${wikiLink}</a>`;
+  }
+  if (mainCommand == "Error") {
+    cr_popup("--log", {
+      subject: "Error occured",
+      text: `<hr /> You have made an"${mainContext}" <hr>  Error has been occured at <span class="bg-yl">${Date()}</span>.${wiki}`,
+      button: "Go back",
+    });
+  }
+  document.querySelector(".--log div a").addEventListener("click", () => {
+    _close_popup();
     callback();
   });
 }
-function pop_up_close(el) {
-  leave_animate(el);
-  leave_animate(document.querySelector(".overlay"));
-}
-pop_up_choice(
-  "redirecting",
-  "You will be redirected, be sure that the link is safe.",
-  "come back",
-  "go",
-  () => {
-    alerter("Thanks for filling up the requiremetn");
-  }
-);
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-# DEV 
+# DEV
 
 <div style="display: flex; justify-content: space-between;"><img src="https://scontent.fcai22-4.fna.fbcdn.net/v/t39.30808-6/354262053_118580681263551_2140061046569093118_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0wE09JkJ4IEAX_nSkEJ&_nc_ht=scontent.fcai22-4.fna&oh=00_AfAVwXmN-VKeD2qbsHoZR7WoZqebPwzM72xK_jRlwQNRpQ&oe=64941B39" alt="drawing" width="200" style="border-radius: 50%;" />
   <img src="https://scontent.fcai22-4.fna.fbcdn.net/v/t39.30808-6/352563104_119099257878360_5006024896091772594_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=0wPDyupSVkYAX-PYqDJ&_nc_ht=scontent.fcai22-4.fna&oh=00_AfBQ0LCs36CJfhi3rqzOMC_ZMHF9ciEQs0WyjZFfrAknQg&oe=6494CC60" alt="drawing" width="200" />
 </div>
 
-## Anim
+## Animate-it@1.3.2
 
 ---
 
@@ -12,6 +12,8 @@
   - [Download](#Download)
   - [NPM-Installation](#NPM-Installation)
 - [Docs](#Docs)
+- [Feature](#Features)
+- [Contact](#Contact)
 
 ---
 
@@ -22,13 +24,8 @@
 - First of all, you are going to include the js file `anim.min.js`, or rather `anim.js` for developers.
   Then, you are going to add the `anim.min.css`, or rather `anim.css` for developers which is the style file.
 
-- Also, there are various versions of styles you can use them, you can find them in the `css/themes/` folder. You can try them.
-  See them below:
-
-<img src="preview/preview_theme-default.png" style="width: 49%;">
-<img src="preview/preview_theme-dark.png" style="width: 49%;">
-
-- You just need to put the theme file after the `anim.css` or `anim.min.css` file.
+> If you came from the previous versions, and you wonder where are the theme files, the answer is that, we have changed it.
+> Now, if you want to include any theme file, just write it in `data-animate-style` as an attribute for the body element.
 
 ---
 
@@ -40,28 +37,85 @@
 
 - Then include the animate style `<link type="stylesheet" href="./node_modules/dev-animate/css/anim.min.css" />`.
 
-- Also you can include any theme you want from the `./node_modules/dev-animate/css/themes/` folder
-
 ---
 
 ### Docs
 
 There are various built-in functions in this library.
 
-> ##### An important tip
->
-> You can use `animate.js` file only, if you just need the animations,
-> but if you need to use the `alerters` and the `pop-ups`. Please consider the `anim.min.js` file.
-
-1 - `pop_up_choice(subject (optional), text (optional), btn1_context (optional), btn2_context (optional), callback())`. this function is made to make a pop up window
+1 - `_popup_choice({subject, text, button_1, button_2}, callback())`. this function is made to make a pop-up window
 and you can add callback function, which will be called when the second action `confirm` is made like in
-
-<img src="preview/preview_theme-default.png" style="width: 49%">
 
 > You cannot pass any data through the `pop_up_choice()`. And It will appear with the default text.
 
-2 - `alerter(massage)`. Which is an enhanced alerter instead of the `window.alert(massage)`, because `window.alert(massage)`
-is a sync process.
+2 - `_alert(massage, duration = 1000)`. Which is an enhanced alerter instead of the `window.alert(massage)`, because `window.alert(massage)` is a sync process.
 
-3- `pop_up(subject (optional), text (optional), btn1_context (optional))`. It is like `pop_up_choice()`, but with no return,
+3 - `_confirm_alert({subject, text, button})`. It is like `pop_up_choice()`, but with no return,
 with only one choice. Actually, you can say that it is a toggle alerter.
+
+4 - `_notify(massage, options = {})`. It is a alert, but with different style, and with timer-progress bar.
+
+> Options Object has two properties.
+> **onHoverPause**, which is the hover pause action. Its default is true
+> **Duration**. And its default is 5000ms
+
+5 - `_log(command, callback())`. Actually, it is one of the most incredible functions, you will ever see. It is like the `header()` function in php. You pass a certain command such as:
+
+```
+_log("Error: Invalid Input", () => {
+  _notify("Try resubmitting the form");
+})
+```
+
+Actually, there are no commands right now, maybe in the next versions. Also there are the wiki command, like:
+
+```
+_log("Error: Forbidden URL, wiki: https://mywebsite.com/support", () => {
+  _alert("Try in another time");
+});
+```
+
+##### Next, we have the animation functions
+
+1 - `_begin_animate(element, delay = 1)`. Which will animate the element to enter the screen. Just make that element hidden. And it will make anything. Because in the beggining of the animation, it will give the element high zIndex, block display, and low opacity. Then, it will animate it.
+
+2 - `_leave_animate(element, delay = 1)`. It is the opposite of `_begin_animate`.
+
+### Features
+
+1 - **Optional properties**. Now, every single property is optional. Now, if you passed an empty data object for
+any function, it will work such as
+
+```
+_prompt({}, (txt) => {
+  console.log(txt)
+});
+```
+
+> Also, it is recommended to pass through the data object
+
+```
+var data = {
+  subject: "Confirming",
+  text: "Please confirm the form",
+  button: "Ok",
+}
+```
+
+2 - **Various Styles**. You can customize your styles by passing different arguments to the `data-animate-style` attribute
+for the body element
+
+```
+<!-- Example -->
+<body data-animate-style="circles dark"></body>
+```
+
+> Actually, we have only those two arguments. We will add more in the next versions.
+
+### Contact
+
+- [Facebook](https://www.facebook.com/profile.php?id=100093348596605)
+- [GitHub](https://github.com/devcompany-public)
+- [NPM](https://www.npmjs.com/~dev-company)
+
+- Donate: [Come and buy me a pizza](https://bmc.link/devcompany)
