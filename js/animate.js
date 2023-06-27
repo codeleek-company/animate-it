b35667851fd8e05896713ce7eefb755ebb4f7995
devcompany-public <=>
2023-06-27 22:56:30 +0300

    @v1.3.2

--- js/animate.js
+++ js/animate.js
@@ -1,14 +1,14 @@
-function animate(el, duration = 2000) {
-  begin_animate(el);
-  leave_animate(el, duration);
+function _animate(el, duration = 2000) {
+  _begin_animate(el);
+  _leave_animate(el, duration);
 }
-function begin_animate(el, delay = 1) {
+function _begin_animate(el, delay = 1) {
   el.classList.add("--reset");
   setTimeout(() => {
     el.classList.add("--happening");
   }, delay);
 }
-function leave_animate(el, delay = 1) {
+function _leave_animate(el, delay = 1) {
   setTimeout(() => {
     el.classList.add("--leaving");
     setTimeout(() => {
